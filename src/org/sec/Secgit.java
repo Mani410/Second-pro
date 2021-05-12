c840115624c063a40774a4558b080e20a5d8c6dc
Mani410 <mani78964698@gmail.com>
2021-05-11 17:30:10 -0700

    My second project

---
+++ src/org/sec/Secgit.java
@@ -0,0 +1,13 @@
+package org.sec;
+
+public class Secgit {
+	private void Employee1() {
+		System.out.println("In Delhi");
+
+	}
+	private void Employee2() {
+		System.out.println("In Mumbai");
+
+	}
+
+}
